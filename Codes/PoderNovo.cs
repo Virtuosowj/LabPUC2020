786aafd17c9eb45b535d8e930f1151b52defb847
Lucas Franca <56547052+Virtuosowj@users.noreply.github.com>
2020-03-29 17:03:36 -0300

    Alterações - Lucas Franca
    Alterações nos poderes já prontos e criação de um novo, um bouncing beam

---
+++ Codes/PoderNovo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoderNovo : MonoBehaviour
+{
+   
+    void Update()
+
+    {
+  
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
